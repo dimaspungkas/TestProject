99291d96363542eea2264ad5b276c9af359d197c
DimasPungkasP <dimas_pungkas@tos.co.id>
2025-03-08 15:43:51 +0700

    updated

---
+++ Areas/TestAreaBIT/TestAreaBITAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace TestProject.Areas.TestAreaBIT
+{
+    public class TestAreaBITAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "TestAreaBIT";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "TestAreaBIT_default",
+                "TestAreaBIT/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
